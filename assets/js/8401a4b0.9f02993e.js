70dd5008f1db8b4a0b28ddffe6de86db04f109fc
Sebastian-Andrei Brisan <asbri@Sebastian-Andreis-MacBook-Air.local>
2025-05-13 19:46:56 +0300

    Deploy website - based on f7eecc95f0dbc60059d615bf8299f3983d2f7934

---
+++ assets/js/8401a4b0.9f02993e.js
@@ -0,0 +1,32 @@
+"use strict";(self.webpackChunkedumat_58=self.webpackChunkedumat_58||[]).push([[1555],{2286:(e,r,t)=>{t.r(r),t.d(r,{assets:()=>c,contentTitle:()=>o,default:()=>u,frontMatter:()=>l,metadata:()=>n,toc:()=>d});const n=JSON.parse('{"id":"c6/01","title":"01 - Mul\u021bimi de numere","description":"","source":"@site/docs/c6/01.md","sourceDirName":"c6","slug":"/c6/01","permalink":"/curs/docs/c6/01","draft":false,"unlisted":false,"editUrl":"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/docs/c6/01.md","tags":[],"version":"current","sidebarPosition":1,"frontMatter":{"sidebar_position":1,"slug":"/c6/01","description":""},"sidebar":"tutorialSidebar","previous":{"title":"Organigram\u0103","permalink":"/curs/docs/c6/00"},"next":{"title":"02 - Intervale, compara\u021bii \u0219i aproxim\u0103ri","permalink":"/curs/docs/c6/02"}}');var i=t(4848),a=t(8453),s=t(8584);const l={sidebar_position:1,slug:"/c6/01",description:""},o="01 - Mul\u021bimi de numere",c={},d=[{value:"Defini\u021bii",id:"defini\u021bii",level:3},{value:"Reprezentare pe axa numerelor",id:"reprezentare-pe-axa-numerelor",level:3},{value:"Observa\u021bii grafice",id:"observa\u021bii-grafice",level:3},{value:"Observa\u021bii generale",id:"observa\u021bii-generale",level:3}];function m(e){const r={admonition:"admonition",h1:"h1",h3:"h3",header:"header",hr:"hr",li:"li",p:"p",strong:"strong",ul:"ul",...(0,a.R)(),...e.components};return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)(r.header,{children:(0,i.jsx)(r.h1,{id:"01---mul\u021bimi-de-numere",children:"01 - Mul\u021bimi de numere"})}),"\n",(0,i.jsx)(r.hr,{}),"\n",(0,i.jsx)(r.h3,{id:"defini\u021bii",children:"Defini\u021bii"}),"\n",(0,i.jsx)(r.p,{children:"Numere naturale:"}),"\n",(0,i.jsx)(s.A,{children:String.raw`
+\mathbb{N} = \{0,1,2,3,4,5 \dots\}
+`}),"\n",(0,i.jsx)(r.p,{children:"Numere \xeentregi:"}),"\n",(0,i.jsx)(s.A,{children:String.raw`
+\mathbb{Z} = \{\dots\ -5,-4,-3,-2,-1 \dots\}+\mathbb{N}
+`}),"\n",(0,i.jsx)(r.p,{children:"Numere ra\u021bionale:"}),"\n",(0,i.jsx)(s.A,{children:String.raw`
+\mathbb{Q} = \{\dots \frac{2}{3} \dots 5,14 \dots\}+\mathbb{N}+\mathbb{Z}
+`}),"\n",(0,i.jsx)(r.hr,{}),"\n",(0,i.jsx)(r.h3,{id:"reprezentare-pe-axa-numerelor",children:"Reprezentare pe axa numerelor"}),"\n",(0,i.jsx)(s.A,{children:String.raw`
+\overset{{-\infty}}{{\ }} \!\!\!\! \rule{0.75cm}{0.4pt} \!\!\!\!
+\overset{\textcolor{green}{-5}}{{\mid}} \!\!\!\! \rule{1.5cm}{0.4pt} \!\!\!\!
+\overset{\textcolor{green}{-4}}{{\mid}} \!\!\!\! \rule{1.5cm}{0.4pt} \!\!\!\!
+\overset{\textcolor{green}{-3}}{{\mid}} \!\!\!\! \rule{1.5cm}{0.4pt} \!\!\!\!
+\overset{\textcolor{green}{-2}}{{\mid}} \!\!\!\! \rule{1.5cm}{0.4pt} \!\!\!\!
+\overset{\textcolor{green}{-1}}{{\mid}} \!\!\!\! \rule{1.5cm}{0.4pt} \!\!\!\!
+\overset{\textcolor{red}{\mathbf{0}}}{{\mid}} \!\!\!\! \rule{1cm}{0.4pt} \!\!\!\!
+\underset{\textcolor{blue}{\displaystyle\frac{2}{3}}}{{\mid}} \!\!\!\! \rule{0.5cm}{0.4pt} \!\!\!\!
+\overset{\textcolor{red}{1}}{{\mid}} \!\!\!\! \rule{1.65cm}{0.4pt} \!\!\!\!
+\overset{\textcolor{red}{2}}{{\mid}} \!\!\!\! \rule{1.65cm}{0.4pt} \!\!\!\!
+\overset{\textcolor{red}{3}}{{\mid}} \!\!\!\! \rule{1.65cm}{0.4pt} \!\!\!\!
+\overset{\textcolor{red}{4}}{{\mid}} \!\!\!\! \rule{1.65cm}{0.4pt} \!\!\!\!
+\overset{\textcolor{red}{5}}{{\mid}} \!\!\!\! \rule{0.65cm}{0.4pt} \!\!\!\!
+\underset{\textcolor{blue}{5{,}14}}{{\mid}} \!\!\!\! \rule{0.75cm}{0.4pt} \!\!\!\!
+\overset{{+\infty}}{{\ }}
+\\[2em]
+\textcolor{green}{\text{Strict Întregi}} \quad
+\textcolor{red}{\text{Strict Naturale}} \quad
+\textcolor{blue}{\text{Strict Raționale}}
+`}),"\n",(0,i.jsx)(r.hr,{}),"\n",(0,i.jsx)(r.h3,{id:"observa\u021bii-grafice",children:"Observa\u021bii grafice"}),"\n",(0,i.jsxs)(r.ul,{children:["\n",(0,i.jsxs)(r.li,{children:["Deplasarea spre ",(0,i.jsx)(r.strong,{children:"st\xe2nga"})," indic\u0103 valori ",(0,i.jsx)(r.strong,{children:"mai mici"}),"."]}),"\n",(0,i.jsxs)(r.li,{children:["Deplasarea spre ",(0,i.jsx)(r.strong,{children:"dreapta"})," indic\u0103 valori ",(0,i.jsx)(r.strong,{children:"mai mari"}),"."]}),"\n"]}),"\n",(0,i.jsx)(r.hr,{}),"\n",(0,i.jsx)(r.h3,{id:"observa\u021bii-generale",children:"Observa\u021bii generale"}),"\n",(0,i.jsxs)(r.admonition,{title:"aten\u021bie",type:"tip",children:[(0,i.jsx)(s.A,{children:String.raw`\mathbb{N} \subset \mathbb{Z} \subset \mathbb{Q}`}),(0,i.jsx)(s.A,{children:String.raw`
+\begin{aligned}
+& \text{Toate numerele naturale } (\mathbb{N}) \text{ sunt incluse în mulțimea numerelor întregi } (\mathbb{Z}).\\[6pt]
+& \text{Toate numerele întregi } (\mathbb{Z}) \text{ sunt incluse în mulțimea numerelor raționale } (\mathbb{Q}).
+\end{aligned}
+`})]})]})}function u(e={}){const{wrapper:r}={...(0,a.R)(),...e.components};return r?(0,i.jsx)(r,{...e,children:(0,i.jsx)(m,{...e})}):m(e)}},8374:(e,r,t)=>{t.d(r,{A:()=>s});var n=t(6540),i=t(2130);function a(){return(a=Object.assign||function(e){for(var r=1;r<arguments.length;r++){var t=arguments[r];for(var n in t)Object.prototype.hasOwnProperty.call(t,n)&&(e[n]=t[n])}return e}).apply(this,arguments)}const s=(0,n.memo)((function(e){var r=e.children,t=e.math,s=e.block,l=e.errorColor,o=e.renderError,c=e.settings,d=e.as,m=function(e,r){if(null==e)return{};var t,n,i={},a=Object.keys(e);for(n=0;n<a.length;n++)r.indexOf(t=a[n])>=0||(i[t]=e[t]);return i}(e,["children","math","block","errorColor","renderError","settings","as"]),u=d||(s?"div":"span"),h=null!=r?r:t,p=(0,n.useState)({innerHtml:""}),x=p[0],b=p[1];return(0,n.useEffect)((function(){try{var e=i.Ay.renderToString(h,a({displayMode:!!s,errorColor:l,throwOnError:!!o},c));b({innerHtml:e})}catch(e){if(!(e instanceof i.Ay.ParseError||e instanceof TypeError))throw e;b(o?{errorElement:o(e)}:{innerHtml:e.message})}}),[s,h,l,o,c]),"errorElement"in x?x.errorElement:n.createElement(u,Object.assign({},m,{dangerouslySetInnerHTML:{__html:x.innerHtml}}))}))},8584:(e,r,t)=>{t.d(r,{A:()=>a});t(6540);var n=t(8374),i=t(4848);function a({children:e}){return(0,i.jsx)(n.A,{block:!0,math:e})}}}]);
