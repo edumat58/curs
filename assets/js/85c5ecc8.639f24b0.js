301e9c3d6d9039b844e8fb40383a0d2f61fbe4ba
DeusSebyum11724 <DeusSebyum11724@users.noreply.github.com>
2025-12-07 12:27:38 +0000

    deploy: 24c54ab9bd0388ddf52611b94fcdb6bb0d8ab56f

---
+++ assets/js/85c5ecc8.639f24b0.js
@@ -0,0 +1,79 @@
+"use strict";(self.webpackChunkedumat_58=self.webpackChunkedumat_58||[]).push([[2639],{8374:(e,r,n)=>{n.d(r,{A:()=>l});var i=n(6540),a=n(2130);function t(){return(t=Object.assign||function(e){for(var r=1;r<arguments.length;r++){var n=arguments[r];for(var i in n)Object.prototype.hasOwnProperty.call(n,i)&&(e[i]=n[i])}return e}).apply(this,arguments)}const l=(0,i.memo)((function(e){var r=e.children,n=e.math,l=e.block,d=e.errorColor,u=e.renderError,c=e.settings,o=e.as,s=function(e,r){if(null==e)return{};var n,i,a={},t=Object.keys(e);for(i=0;i<t.length;i++)r.indexOf(n=t[i])>=0||(a[n]=e[n]);return a}(e,["children","math","block","errorColor","renderError","settings","as"]),p=o||(l?"div":"span"),x=null!=r?r:n,m=(0,i.useState)({innerHtml:""}),h=m[0],g=m[1];return(0,i.useEffect)((function(){try{var e=a.Ay.renderToString(x,t({displayMode:!!l,errorColor:d,throwOnError:!!u},c));g({innerHtml:e})}catch(e){if(!(e instanceof a.Ay.ParseError||e instanceof TypeError))throw e;g(u?{errorElement:u(e)}:{innerHtml:e.message})}}),[l,x,d,u,c]),"errorElement"in h?h.errorElement:i.createElement(p,Object.assign({},s,{dangerouslySetInnerHTML:{__html:h.innerHtml}}))}))},8584:(e,r,n)=>{n.d(r,{A:()=>t});n(6540);var i=n(8374),a=n(4848);function t({children:e}){return(0,a.jsx)(i.A,{block:!0,math:e})}},8621:(e,r,n)=>{n.r(r),n.d(r,{assets:()=>c,contentTitle:()=>u,default:()=>p,frontMatter:()=>d,metadata:()=>i,toc:()=>o});const i=JSON.parse('{"id":"c6/modul-2/10","title":"C15 - Reguli de calcul cu puteri","description":"","source":"@site/docs/c6/modul-2/10.mdx","sourceDirName":"c6/modul-2","slug":"/c6/modul-2/10","permalink":"/curs/docs/c6/modul-2/10","draft":false,"unlisted":false,"tags":[],"version":"current","sidebarPosition":11,"frontMatter":{"sidebar_position":11,"slug":"/c6/modul-2/10","description":""},"sidebar":"tutorialSidebar","previous":{"title":"Modul 2","permalink":"/curs/docs/category/modul-2-1"},"next":{"title":"C14 - Puterea cu exponent num\u0103r natural a unui num\u0103r \xeentreg nenul","permalink":"/curs/docs/c6/modul-2/9"}}');var a=n(4848),t=n(8453),l=n(8584);const d={sidebar_position:11,slug:"/c6/modul-2/10",description:""},u="C15 - Reguli de calcul cu puteri",c={},o=[{value:"No\u021biuni de baz\u0103",id:"no\u021biuni-de-baz\u0103",level:2},{value:"(re) | Opusul \u0219i inversul unui num\u0103r",id:"re--opusul-\u0219i-inversul-unui-num\u0103r",level:3},{value:"Reguli fundamentale",id:"reguli-fundamentale",level:2},{value:"R1. \xcenmul\u021birea a dou\u0103 puteri cu aceea\u0219i baz\u0103",id:"r1-\xeenmul\u021birea-a-dou\u0103-puteri-cu-aceea\u0219i-baz\u0103",level:3},{value:"R2. \xcemp\u0103r\u021birea a dou\u0103 puteri cu aceea\u0219i baz\u0103",id:"r2-\xeemp\u0103r\u021birea-a-dou\u0103-puteri-cu-aceea\u0219i-baz\u0103",level:3},{value:"R3. Puterea unui num\u0103r ridicat la putere",id:"r3-puterea-unui-num\u0103r-ridicat-la-putere",level:3},{value:"R4. Puterea unui produs",id:"r4-puterea-unui-produs",level:3},{value:"R5. Ridicarea la putere negativ\u0103",id:"r5-ridicarea-la-putere-negativ\u0103",level:3},{value:"R5.1. Un num\u0103r \xeenmul\u021bit cu o putere negativ\u0103",id:"r51-un-num\u0103r-\xeenmul\u021bit-cu-o-putere-negativ\u0103",level:3},{value:"R6. Puterea unui raport",id:"r6-puterea-unui-raport",level:3}];function s(e){const r={admonition:"admonition",code:"code",h1:"h1",h2:"h2",h3:"h3",header:"header",hr:"hr",li:"li",p:"p",ul:"ul",...(0,t.R)(),...e.components};return(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(r.header,{children:(0,a.jsx)(r.h1,{id:"c15---reguli-de-calcul-cu-puteri",children:"C15 - Reguli de calcul cu puteri"})}),"\n",(0,a.jsx)(r.hr,{}),"\n",(0,a.jsx)(r.h2,{id:"no\u021biuni-de-baz\u0103",children:"No\u021biuni de baz\u0103"}),"\n",(0,a.jsxs)(r.admonition,{title:"Defini\u021bie 1",type:"danger",children:[(0,a.jsxs)(r.p,{children:["Puterea de exponent ",(0,a.jsx)(r.code,{children:"x"})," a unui num\u0103r ",(0,a.jsx)(r.code,{children:"a"}),", numit baz\u0103, este o opera\u021bie \xeentre aceste numere."]}),(0,a.jsxs)(r.ul,{children:["\n",(0,a.jsxs)(r.li,{children:["Dac\u0103 ",(0,a.jsx)(r.code,{children:"x"})," este un ",(0,a.jsx)(r.code,{children:"num\u0103r natural"}),", atunci ridicarea la putere poate fi definit\u0103 ca o ",(0,a.jsx)(r.code,{children:"\xeenmul\u021bire repetat\u0103"}),", notat\u0103:"]}),"\n"]}),(0,a.jsx)(l.A,{children:String.raw`\Large a^x`})]}),"\n",(0,a.jsx)(l.A,{children:String.raw`
+\\[20pt]
+\Large a^x = \underbrace{a \cdot a \cdot \dots \cdot a}_{x\ \text{factori}}
+\\[20pt]
+`}),"\n",(0,a.jsx)(r.admonition,{title:"Defini\u021bie 2",type:"danger",children:(0,a.jsxs)(r.ul,{children:["\n",(0,a.jsxs)(r.li,{children:["Dac\u0103 ",(0,a.jsx)(r.code,{children:"x"})," este un ",(0,a.jsx)(r.code,{children:"num\u0103r negativ"}),", atunci ridicarea la o putere negativ\u0103 corespunde ",(0,a.jsx)(r.code,{children:"inversului"})," puterii pozitive, notat\u0103:","\n",(0,a.jsx)(l.A,{children:String.raw`
+\Large a^{-x} = \frac{1}{a^{\,x}}
+`}),"\n"]}),"\n"]})}),"\n",(0,a.jsx)(l.A,{children:String.raw`
+\\[20pt]
+\Large a^{-x}=\frac{1}{\underbrace{a \cdot a \cdot \dots \cdot a}_{x\ \text{factori}}}
+\\[20pt]
+`}),"\n",(0,a.jsxs)(r.admonition,{title:"Ne Reamintim",type:"tip",children:[(0,a.jsx)(r.h3,{id:"re--opusul-\u0219i-inversul-unui-num\u0103r",children:"(re) | Opusul \u0219i inversul unui num\u0103r"}),(0,a.jsx)("div",{style:{display:"flex",gap:"2rem",flexWrap:"wrap"},children:(0,a.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+&\textbf{Opusul} \text{ unui număr }a\in\mathbb{R}\\[10pt]
+&\text{este: }\boldsymbol{-a \Rightarrow \quad a+(-a)=0}\\[10pt]
+&\textbf{Inversul} \text{ unui număr }a\in\mathbb{R}\setminus\{0\}\\[10pt]
+&\text{este: }\boldsymbol{\frac{1}{a} \Rightarrow \quad a\cdot\frac{1}{a}=1}.
+\end{aligned}
+`})})]}),"\n",(0,a.jsx)(r.hr,{}),"\n",(0,a.jsx)(r.h2,{id:"reguli-fundamentale",children:"Reguli fundamentale"}),"\n",(0,a.jsx)(r.admonition,{title:"Foarte Important",type:"danger",children:(0,a.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+&\Large\bf{a^{0}=1.}
+\end{aligned}
+`})}),"\n",(0,a.jsx)("div",{style:{display:"flex",gap:"2rem",flexWrap:"wrap"},children:(0,a.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+&\large\text{Pentru orice }a,b,c\in\mathbb{R}\text{ și }x,y\in\mathbb{Z}\\[10pt]
+&\large\text{se definesc următoarele: }
+\end{aligned}
+`})}),"\n",(0,a.jsx)(r.hr,{}),"\n",(0,a.jsx)(r.h3,{id:"r1-\xeenmul\u021birea-a-dou\u0103-puteri-cu-aceea\u0219i-baz\u0103",children:"R1. \xcenmul\u021birea a dou\u0103 puteri cu aceea\u0219i baz\u0103"}),"\n",(0,a.jsx)("div",{style:{display:"flex",gap:"2rem",flexWrap:"wrap"},children:(0,a.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+&\boldsymbol{a^x \cdot a^y = a^{(x+y)}}\\[20pt]
+&\text{exemplu: }2^7 \cdot 2^9 = 2^{(7+9)} = 2^{16}\\
+\end{aligned}
+`})}),"\n",(0,a.jsx)(r.hr,{}),"\n",(0,a.jsx)(r.h3,{id:"r2-\xeemp\u0103r\u021birea-a-dou\u0103-puteri-cu-aceea\u0219i-baz\u0103",children:"R2. \xcemp\u0103r\u021birea a dou\u0103 puteri cu aceea\u0219i baz\u0103"}),"\n",(0,a.jsx)("div",{style:{display:"flex",gap:"2rem",flexWrap:"wrap"},children:(0,a.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+&\boldsymbol{a^x : a^y = a^{(x-y)}}\\[20pt]
+&\text{exemplu: }2^{12} : 2^7 = 2^{(12-7)} = 2^5\\
+\end{aligned}
+`})}),"\n",(0,a.jsx)(r.hr,{}),"\n",(0,a.jsx)(r.h3,{id:"r3-puterea-unui-num\u0103r-ridicat-la-putere",children:"R3. Puterea unui num\u0103r ridicat la putere"}),"\n",(0,a.jsx)("div",{style:{display:"flex",gap:"2rem",flexWrap:"wrap"},children:(0,a.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+&\boldsymbol{(a^x)^y = a^{(x\; \cdot \; y)}}\\[20pt]
+&\text{exemplu: }(7^4)^5 = 7^{(4\; \cdot \; 5)} = 7^{20} \\
+\end{aligned}
+`})}),"\n",(0,a.jsx)(r.hr,{}),"\n",(0,a.jsx)(r.h3,{id:"r4-puterea-unui-produs",children:"R4. Puterea unui produs"}),"\n",(0,a.jsx)("div",{style:{display:"flex",gap:"2rem",flexWrap:"wrap"},children:(0,a.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+&\boldsymbol{(a \cdot b)^x = a^x \cdot b^x}\\[20pt]
+&\text{exemplu: }(5 \cdot 7)^2=5^2 \cdot 7^2 \\
+\end{aligned}
+`})}),"\n",(0,a.jsx)(r.hr,{}),"\n",(0,a.jsx)(r.h3,{id:"r5-ridicarea-la-putere-negativ\u0103",children:"R5. Ridicarea la putere negativ\u0103"}),"\n",(0,a.jsx)("div",{style:{display:"flex",gap:"2rem",flexWrap:"wrap"},children:(0,a.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+&\boldsymbol{a^{-x} \;=\;\frac{1}{a^{x}}}\\[20pt] 
+&\text{exemplu: }10^{-3}=\frac{1}{10^3} \\
+\end{aligned}
+`})}),"\n",(0,a.jsx)(r.hr,{}),"\n",(0,a.jsx)(r.h3,{id:"r51-un-num\u0103r-\xeenmul\u021bit-cu-o-putere-negativ\u0103",children:"R5.1. Un num\u0103r \xeenmul\u021bit cu o putere negativ\u0103"}),"\n",(0,a.jsx)("div",{style:{display:"flex",gap:"2rem",flexWrap:"wrap"},children:(0,a.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+&\boldsymbol{c\cdot a^{-x}
+\;=\;c\cdot\frac{1}{a^x}
+\;=\;\frac{c}{a^x}}\\[20pt]
+&\text{exemplu: }3\cdot10^{-3}=\frac{3}{10^3} \\[20pt]
+&\text{și invers: } \frac{7}{29} = 7 \cdot \frac{1}{29} = 7 \cdot 29^{-1} \\[20pt]
+\end{aligned}
+`})}),"\n",(0,a.jsx)(r.admonition,{title:"Not\u0103 general\u0103",type:"info",children:(0,a.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+&\frac{p}{q}
+=p\cdot\frac{1}{q}
+=p\,q^{-1},\quad p,q\in\mathbb{Z},\,q\neq0. \\[8pt]
+\end{aligned}
+`})}),"\n",(0,a.jsx)(r.hr,{}),"\n",(0,a.jsx)(r.h3,{id:"r6-puterea-unui-raport",children:"R6. Puterea unui raport"}),"\n",(0,a.jsx)("div",{style:{display:"flex",gap:"2rem",flexWrap:"wrap"},children:(0,a.jsx)(l.A,{children:String.raw`
+\begin{aligned}
+&\boldsymbol{\Large\Bigl(\tfrac{a}{b}\Bigr)^{x}
+\normalsize=\frac{a^{x}}{b^{x}}
+=a^{x}\!:\!b^{x}
+=a^{x}\cdot \frac{1}{b^{x}},
+=a^{x}\cdot b^{-x}} \\[20pt]
+&\text{exemplu: }\Large\Bigl(\tfrac{3}{17}\Bigr)^2
+\normalsize=\frac{3^2}{17^2}
+=3^2 : 17^2
+=3^2 \cdot \frac{1}{17^2}
+=3^2\cdot17^{-2}. \\[10pt]
+\end{aligned}
+`})}),"\n",(0,a.jsx)(r.hr,{})]})}function p(e={}){const{wrapper:r}={...(0,t.R)(),...e.components};return r?(0,a.jsx)(r,{...e,children:(0,a.jsx)(s,{...e})}):s(e)}}}]);
